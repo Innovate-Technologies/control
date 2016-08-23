ad4600f4e1050a567fe99cb13a5ad1fe4d97efe6
Maartje Eyskens <meyskens@me.com>
2016-08-24 00:16:42 +0200

    Add cast update warning

--- src/manage/manage-ctrl.js
+++ src/manage/manage-ctrl.js
@@ -1,4 +1,18 @@
-export default /*@ngInject*/ function ($rootScope, service, $scope, ManageService) {
-  $rootScope.service = service;
+export default /*@ngInject*/ function ($rootScope, service, $scope, ManageService, AboutService, ConfigService) {
+  $scope.service = service;
+  if (service.group.toLowerCase().indexOf("node") !== -1) {
+    ConfigService.getConfig().then((config) => {
+      this.castRevision = config.version.Cast;
+      checkCastVersion();
+    });
+  }
   $scope.sections = ManageService.getSections();
+
+  const checkCastVersion = () => {
+    AboutService.getCastBuildInfo().then(({ version: revision }) => {
+      if (revision !== this.castRevision) {
+        $scope.castNeedsUpdate = true;
+      }
+    });
+  };
 }
