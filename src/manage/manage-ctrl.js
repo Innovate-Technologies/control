0b5169a8fa547b190136d411aaab36247ae444e4
Maartje Eyskens <meyskens@me.com>
2016-08-24 11:19:26 +0200

    Fix feedback

--- src/manage/manage-ctrl.js
+++ src/manage/manage-ctrl.js
@@ -1,5 +1,5 @@
 export default /*@ngInject*/ function ($rootScope, service, $scope, ManageService, AboutService, ConfigService) {
-  $scope.service = service;
+  $rootScope.service = service;
   if (service.group.toLowerCase().indexOf("node") !== -1) {
     ConfigService.getConfig().then((config) => {
       this.castRevision = config.version.Cast;
@@ -10,9 +10,7 @@ export default /*@ngInject*/ function ($rootScope, service, $scope, ManageServic
 
   const checkCastVersion = () => {
     AboutService.getCastBuildInfo().then(({ version: revision }) => {
-      if (revision !== this.castRevision) {
-        $scope.castNeedsUpdate = true;
-      }
+      $scope.castNeedsUpdate = revision !== this.castRevision;
     });
   };
 }
