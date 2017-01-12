d570394ecdec63200f61128ae396139a09b00863
Maartje Eyskens <meyskens@me.com>
2017-01-12 14:35:38 +0100

    Add custom domain option under links

--- src/manage/cast/config-service.js
+++ src/manage/cast/config-service.js
@@ -73,6 +73,16 @@ export default class ConfigService {
         .then(response => response.data);
     };
 
+    /**
+     * Set custom domain
+     * @param {String} domain - The domain name to use
+     */
+    this.setCustomDomain = (domain) => {
+      const username = $rootScope.service.username;
+      return $http.put(`${ENV.apiEndpoint}/control/cast/custom-domain/${username}`, { domain })
+        .then(response => response.data);
+    };
+
     $rootScope.$on("invalidate-cast-config-cache", this.invalidateCache);
   }
 }
--- src/manage/cast/links/links-ctrl.js
+++ src/manage/cast/links/links-ctrl.js
@@ -1,6 +1,32 @@
 import { lodash as _, angular } from "../../../vendor";
 
-export default /*@ngInject*/ function (config) {
+export default /*@ngInject*/ function (config, ConfigService, $alert, $scope) {
   this.config = angular.copy(config);
   this.primaryStream = _.find(config.streams, { primary: true }).stream;
+  this.useCustomDomain = !(/.*radioca.st$/.test(config.hostname));
+  this.useCustomDomain ? this.customDomain = config.hostname.replace("https://", "") : this.customDomain = "";
+
+  this.isValidDomain = (domain) => {
+    return /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/.test(domain);
+  };
+
+  this.setCustomDomain = () => {
+    this.isSaving = true;
+    return ConfigService.setCustomDomain(this.customDomain).then(() => {
+      $alert({
+        content: "Successfully changed the domain.",
+        type: "success",
+        duration: 3,
+      });
+      $scope.$emit("invalidate-cast-config-cache");
+      this.isSaving = false;
+    }, () => {
+      $alert({
+        content: "Failed to update the domain. Double check your DNS and try again later.",
+        type: "danger",
+        duration: 5,
+      });
+      this.disableForm = false;
+    });
+  };
 }
