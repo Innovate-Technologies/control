cd497cf058ddc51a66333f8de84f439e54b510a7
Maartje Eyskens <maartje@eyskens.me>
2019-06-07 07:41:54 +0000

    Remove extraApps

--- src/manage/extra-services/index.js
+++ src/manage/extra-services/index.js
@@ -1,12 +1,10 @@
 import { angular } from "../../vendor";
 
-import extraApps from "./apps";
 import extraNpTweets from "./now-playing-tweets";
 import extraTuneIn from "./tunein-integration";
 import extraPlayer from "./player";
 
 export default angular.module("control.manage.extra-services", [
-  extraApps,
   extraNpTweets,
   extraTuneIn,
   extraPlayer,
